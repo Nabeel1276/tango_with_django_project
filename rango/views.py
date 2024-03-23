397929074aa081a59dcd35aafc58f77cce9d4198
Nabeel Khan <nabeel.khan@bbc.co.uk>
2024-03-23 17:47:26 +0000

    fixed 1 test for error message when adding a category that pre-xists

--- rango/views.py
+++ rango/views.py
@@ -40,9 +40,9 @@ def add_category(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-        return redirect('/rango/')
-    else:
-        print(form.errors)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
 def add_page(request, category_name_slug):
